b32e3ebb62131beed9bd2341ad6e4d2ae246a92b
ankush <ankush.codenote@gmail.com>
2022-11-21 17:43:42 +0530

    method or ruby program

---
+++ mthod.rb
@@ -0,0 +1,20 @@
+def area(length ,width)
+	length*width        #method with me argument
+  
+end
+
+
+def area                # method with the no aregument given before
+	length = 250
+	return "return method" #return statment will stop the upcoming execution
+	width = 30
+	length*width
+	
+end
+
+
+def area (length = 20,width = 30)
+  length*width	
+end
+
+
